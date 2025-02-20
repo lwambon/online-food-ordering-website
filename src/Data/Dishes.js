6ec37118db0a876de8a4ac113110547f83f34f35
lwambon <lwambonyaga@gmail.com>
2025-02-20 19:35:42 +0300

    menu changes

--- src/Data/Dishes.js
+++ src/Data/Dishes.js
@@ -11,15 +11,15 @@ import Dish9 from "../assets/Kadai Paneer.jpeg";
 import Dish10 from "../assets/mushroom tikka.jpeg";
 
 const dishes = [
-  { id: 1, image: Dish1, name: "Grilled Chicken" },
-  { id: 2, image: Dish2, name: "Spaghetti Carbonara" },
-  { id: 3, image: Dish3, name: "Vegetable Stir Fry" },
-  { id: 4, image: Dish4, name: "Beef Steak" },
-  { id: 5, image: Dish5, name: "Sushi Platter" },
-  { id: 6, image: Dish6, name: "Seafood Pasta" },
-  { id: 7, image: Dish7, name: "BBQ Ribs" },
-  { id: 8, image: Dish8, name: "Mango Avocado Salad" },
-  { id: 9, image: Dish9, name: "Chocolate Lava Cake" },
-  { id: 10, image: Dish10, name: "Pancakes with Berries" },
+  { id: 1, image: Dish1, name: "burger" },
+  { id: 2, image: Dish2, name: "cheese nachos" },
+  { id: 3, image: Dish3, name: "chalo bhatura" },
+  { id: 4, image: Dish4, name: "taco" },
+  { id: 5, image: Dish5, name: "garlic bread" },
+  { id: 6, image: Dish6, name: "Jeera" },
+  { id: 7, image: Dish7, name: "pastries" },
+  { id: 8, image: Dish8, name: "Honey Chilli potatoes" },
+  { id: 9, image: Dish9, name: "Kadai paneer" },
+  { id: 10, image: Dish10, name: "Mushroom tikka" },
 ];
 export default dishes;
